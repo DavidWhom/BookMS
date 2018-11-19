ea393ca196f39a6941309950fb6474d6e068f421
David <997740098@qq.com>
2018-11-19 20:21:31 +0800

    测试代码修改后提交到github

--- src/com/sa/dao/AjaxDao.java
+++ src/com/sa/dao/AjaxDao.java
@@ -6,7 +6,7 @@ import com.sa.domain.AjaxFormData;
 
 public interface AjaxDao {
 	public List<AjaxFormData> getCollege();
-	
+	public void testCommit2git();
 	public List<AjaxFormData> getMajor(String college);
 	
 	public List<AjaxFormData> getClasses(String major);
--- src/com/sa/dao/impl/AjaxDaoImpl.java
+++ src/com/sa/dao/impl/AjaxDaoImpl.java
@@ -3,6 +3,7 @@ package com.sa.dao.impl;
 import java.util.List;
 
 import com.sa.dao.AjaxDao;
+import com.sa.dao.String;
 import com.sa.domain.AjaxFormData;
 
 public class AjaxDaoImpl extends BaseDao<AjaxFormData> implements AjaxDao {
@@ -23,6 +24,36 @@ public class AjaxDaoImpl extends BaseDao<AjaxFormData> implements AjaxDao {
 	public List<AjaxFormData> getClasses(String major) {
 		String sql = "SELECT DISTINCT classes FROM user_info WHERE major = ?";
 		return queryForList(sql, major);
+	}
+
+	@Override
+	public com.sa.dao.List<AjaxFormData> queryForList(String sql, Object... args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public com.sa.dao.List<AjaxFormData> getCollege() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void testCommit2git() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public com.sa.dao.List<AjaxFormData> getMajor(String college) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public com.sa.dao.List<AjaxFormData> getClasses(String major) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
